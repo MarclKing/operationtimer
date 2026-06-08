d014692adebea775a21c50ff2d6d3b4c4af8feb3
Marcl King <iPunched@gmail.com>
2026-06-08 21:00:30 +0200

    Release2

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -748,7 +748,7 @@ class ScheduleScreenState extends State<ScheduleScreen> {
       }
     }
   });
-  ScheduleScreenState.pushScheduleToWidget(); ← neu
+  ScheduleScreenState.pushScheduleToWidget(); 
 }
 
   void _setMonth(DateTime month) {
