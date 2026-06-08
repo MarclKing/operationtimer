e5893ce68376b8a1c506260abc00718803853293
Marcel <marcel@MacBook-Air-von-Marcel.local>
2026-06-08 13:54:40 +0200

    Release1

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,35 +4,37 @@ import WidgetKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
 
-    let controller = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(
-      name: "de.marcel.optimes/widget",
-      binaryMessenger: controller.binaryMessenger
-    )
+        let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
 
-    channel.setMethodCallHandler { call, result in
-      if call.method == "updateSchedule" {
-        if let args = call.arguments as? [String: Any],
-           let json = args["json"] as? String {
-          let defaults = UserDefaults(suiteName: "group.de.marcel.optimes")
-          defaults?.set(json, forKey: "schedule_entries")
-          defaults?.synchronize()
+        if let controller = window?.rootViewController as? FlutterViewController {
+            let channel = FlutterMethodChannel(
+                name: "de.marcel.optimes/widget",
+                binaryMessenger: controller.binaryMessenger
+            )
+            channel.setMethodCallHandler { call, result in
+                if call.method == "updateSchedule" {
+                    if let args = call.arguments as? [String: Any],
+                       let json = args["json"] as? String {
+                        let defaults = UserDefaults(suiteName: "group.de.marcel.optimes")
+                        defaults?.set(json, forKey: "schedule_entries")
+                        defaults?.synchronize()
+                    }
+                    if #available(iOS 14.0, *) {
+                        WidgetCenter.shared.reloadAllTimelines()
+                    }
+                    result(nil)
+                } else {
+                    result(FlutterMethodNotImplemented)
+                }
+            }
         }
-        if #available(iOS 14.0, *) {
-          WidgetCenter.shared.reloadAllTimelines()
-        }
-        result(nil)
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
+
+        return result
     }
-
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
